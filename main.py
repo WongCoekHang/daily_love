6a9d5fb08a6b5d1d02213df5e90f08280ee43317
WongCoekHang <86662776+WongCoekHang@users.noreply.github.com>
2022-08-16 22:35:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ if wea.__contains__("雷阵雨"):
   
 data = {"city":{"value":city},"weather_remark":{"value":weather_remark},"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
-#resw = wm.send_template(userwu_id, template_id, data)
-#resxw = wm.send_template(userxiuwu_id, template_id, data)
+resw = wm.send_template(userwu_id, template_id, data)
+resxw = wm.send_template(userxiuwu_id, template_id, data)
 print(res)
-#print(resw)
-#print(resxw)
+print(resw)
+print(resxw)
